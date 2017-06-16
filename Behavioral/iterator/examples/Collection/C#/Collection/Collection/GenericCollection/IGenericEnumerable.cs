4134db73749512a04481e9cf6dbcc28f3454a7a8
ViridRaven <virid.raven@gmail.com>
2017-06-16 23:43:41 +0500

    Added c# example

---
+++ Behavioral/iterator/examples/Collection/C#/Collection/Collection/GenericCollection/IGenericEnumerable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collection.GenericCollection
+{
+    interface IGenericEnumerable
+    {
+        IGenericEnumerator GetEnumerator();
+    }
+}
